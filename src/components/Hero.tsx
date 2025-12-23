748b60460c4f67b423c9f90351b10d15b7547081
Aditya Sharma <adityasharma4510@gmail.com>
2025-12-23 21:30:04 +0530

    navlink deployment issue fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
